e932377232d5aed1051ddb504f0922a54433c7a5
Zukto <juliandls@hotmail.com>
2025-11-12 12:50:25 -0300

    Tabulado correcto y correcion de comentarios

--- Assets/js/menu.js
+++ Assets/js/menu.js
@@ -1,4 +1,5 @@
-// menu.js - control menú hamburguesa (puro JS, accesible)
+
+// ----------menu.js - control menú hamburguesa----------
 document.addEventListener('DOMContentLoaded', () => {
 const btn = document.querySelector('.menu-toggle');
 const nav = document.getElementById('main-nav');
@@ -7,7 +8,7 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('open');
 });
 
-// Cierra el menú al hacer clic en un enlace
+// ----------Cierra el menú al hacer clic en un enlace----------
 nav.addEventListener('click', e => {
     if (e.target.tagName === 'A') nav.classList.remove('open');
 });
